219dbb38ae9b1939115ad0b76536618ece2ad5d6
airjlee <airjlee04@gmail.com>
2024-07-18 17:31:19 -0700

    reinit

---
+++ client/src/Main.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Affordhealth
+                </p>
+                <form>
+                    <label>
+ 
+                        <input type="text" name="name" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+            </header>
+        </div>
+    );
+}
+
+export default App;
